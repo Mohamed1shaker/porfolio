9ac2e60d3456f9b708ec82c129ae7f7fb4033458
Mohammed Shaker <112406830+Mohamed1shaker@users.noreply.github.com>
2025-01-07 14:18:57 +0200

    Add files via upload

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+        strings: [
+            "<span>Hi, I am Mohamed Shaker</span> I am FrontEnd developer React , I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things."
+        ],
+        typeSpeed: 7,
+        contentType: 'html'
+    });
+}, 100);
